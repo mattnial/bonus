49b47bf2a0533d6fbec2840dd107f8007cd3ab3a
mattnial <mattnial@hotmail.com>
2026-02-11 12:04:51 -0500

    Delete v4 directory

--- v4/api/admin/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy(); // Destruye el pase
-header("Location: /login.html"); // Manda al login
-exit;
-?>
--- v4/assets/js/config.js
+++
@@ -1,7 +0,0 @@
-// ARCHIVO: assets/js/config.js
-
-// Usamos window.API_URL para evitar el error de "redeclaration"
-// si el archivo llega a cargarse dos veces por accidente.
-if (typeof window.API_URL === 'undefined') {
-    window.API_URL = "api";
-}
